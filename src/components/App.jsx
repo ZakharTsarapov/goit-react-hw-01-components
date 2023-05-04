43b0a2fbebdf3b19d33c4db88ad7fbe7203c1522
ZakharTsarapov <tsarapovs@gmail.com>
2023-05-04 16:03:40 +0300

    done 4 of 4, need styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,23 @@ import { TransactionHistory } from './transactionHistory/TransactionHistory';
 import friends from './FriendsList/friends.json';
 import { FriendList } from './FriendsList/FriendList';
 import css from './App.module.css';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
 
 export const App = () => {
   return (
     <div className={css.container}>
       React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,2 +1,34 @@
 import PropTypes from 'prop-types';
+import { getRandomHexColor } from './randomHex';
 
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section class="statistics">
+      {title && <h2>{title}</h2>}
+
+      <ul class="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            class="item"
+            key={id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span class="label">{label}</span>
+            <span class="percentage">{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      parcentage: PropTypes.number.isRequired,
+    })
+  ),
+  title: PropTypes.string,
+};
